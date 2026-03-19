0b19a6be0e780469d4b2a745c2691fb405e1c424
jcPimcore <jochen.califice@pimcore.com>
2026-03-19 12:49:50 +0100

    [Task] Update composer dependencies, fix stan issues

--- src/PaymentManager/Payment/Datatrans.php
+++ src/PaymentManager/Payment/Datatrans.php
@@ -779,10 +779,10 @@ XML;
     protected function xmlRequest($endpoint, $xml)
     {
         $ch = curl_init($endpoint);
-        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POST, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: text/xml']);
         curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
         $output = curl_exec($ch);
         curl_close($ch);
